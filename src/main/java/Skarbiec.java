5fa4b6e628d96f5170188818f8e1b34f396fc3f2
Patryk M. Bogdan <patryk.m.bogdan@gmail.com>
2026-05-23 10:25:13 +0200

    Gotowe metody w klasie skarbiec

--- src/main/java/Skarbiec.java
+++ src/main/java/Skarbiec.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Skarbiec {
-    private Map<Nominal,Integer> monety;
+    public Map<Nominal,Integer> monety;
 
     void zaladujStartowe(int ilosc){
         this.monety = new HashMap<>();
@@ -12,21 +12,21 @@ public class Skarbiec {
             this.monety.put(nominal, ilosc);
         }
     }
-    List<Nominal> obliczWrzut(double wrzucone){
+    public static List<Nominal> obliczWrzut(double wrzucone){
         List<Nominal> obliczonyWrzut = new ArrayList<>();
-        int wrzuconeGrosze = (int) wrzucone *100;
-        int monetaObliczana = 0;
+        int wrzuconeGrosze = (int) (wrzucone *100);
+        int monetaObliczanaWGroszach = 0;
         for(Nominal nominal : Nominal.values()){
             switch (nominal){
-                case PLN_5_00 -> monetaObliczana = 5*100;
-                case PLN_2_00 -> monetaObliczana = 2*100;
-                case PLN_1_00 -> monetaObliczana = 1*100;
-                case GR_50 -> monetaObliczana = (int) 0.5*100;
-                case GR_20 -> monetaObliczana = (int) 0.2*100;
-                case GR_10 -> monetaObliczana = (int) 0.1*100;
+                case PLN_5_00 -> monetaObliczanaWGroszach = 500;
+                case PLN_2_00 -> monetaObliczanaWGroszach = 200;
+                case PLN_1_00 -> monetaObliczanaWGroszach = 100;
+                case GR_50 -> monetaObliczanaWGroszach = 50;
+                case GR_20 -> monetaObliczanaWGroszach = 20;
+                case GR_10 -> monetaObliczanaWGroszach = 10;
             }
-            while (wrzuconeGrosze - monetaObliczana >= 0){
-                wrzucone -= monetaObliczana;
+            while (wrzuconeGrosze - monetaObliczanaWGroszach >= 0){
+                wrzuconeGrosze -= monetaObliczanaWGroszach;
                 obliczonyWrzut.add(nominal);
             }
             if(wrzucone <= 0) return obliczonyWrzut;
@@ -35,19 +35,22 @@ public class Skarbiec {
     }
 
     void dodajMonetyWrzut(List<Nominal> wrzucone){
-
+        for(Nominal wrzuconaMoneta : wrzucone){
+            int obecnaIlosc = monety.getOrDefault(wrzuconaMoneta, 0);
+            this.monety.put(wrzuconaMoneta, obecnaIlosc + 1);
+        }
     }
-    /*List<Nominal> obliczOptymalnaReszte(double kwota){
-
+    //TODO : Wydawanie reszty niech uwzglednia jakie ma monety
+    public static List<Nominal> obliczOptymalnaReszte(double resztaDoWydania){
+        List<Nominal> monetyReszta = obliczWrzut(resztaDoWydania);
+        return monetyReszta;
     }
-    */
+
     public void wydajMonety(List<Nominal> monetyDoWydania) {
+        for(Nominal wrzuconaMoneta : monetyDoWydania) {
+            int obecnaIlosc = monety.getOrDefault(wrzuconaMoneta, 0);
+            this.monety.put(wrzuconaMoneta, obecnaIlosc - 1);
+        }
 
-    }
-
-    public static void main(String[] args) {
-        Skarbiec test = new Skarbiec();
-        List<Nominal> re = test.obliczWrzut(04.9);
-        System.out.println(re);
     }
 }
