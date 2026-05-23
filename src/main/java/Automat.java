4422fe55ddf592a32b68b56564ff236236e6e1af
Szymon Lakomy <lakomyszymon3@gmail.com>
2026-05-23 17:11:59 +0200

    Uzupelnione klasy Produkt i Transakcja, poprawa w klasie Skarbiec i konstruktor w Automacie ( stworzony obiekt)

--- src/main/java/Automat.java
+++ src/main/java/Automat.java
@@ -4,15 +4,22 @@ public class Automat {
     private int licznikTransakcji;
     private int sprzedaneProdukty;
     private int nieudaneTransakcje;
+
+    public Automat(int czestotliwoscDostaw, Region poziomZamoznosci){
+        this.czestotliwoscDostaw = czestotliwoscDostaw;
+        this.poziomZamoznosci = poziomZamoznosci;
+        this.licznikTransakcji = 0;
+        this.sprzedaneProdukty = 0;
+        this.nieudaneTransakcje = 0;
+    }
+    public static void uruchomSymulacje(int iloscProb, Region region, int dostawyCo, int StartoweMonety){
+        Automat automat_metadruk = new Automat(dostawyCo,region);
+
+    }
     private void sprawdzDostawy(){
 
     }
-
-    public static void uruchomSymulacje(int iloscProb, Region region, int dostawyCo, int StartoweMonety){
-
-    }
     public Transakcja sprobujKupic(Produkt produkt, double wrzuconaKwota){
-        
     }
     public void generujPodsumowanie(){
 
